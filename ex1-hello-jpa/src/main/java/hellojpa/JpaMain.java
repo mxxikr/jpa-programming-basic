10f6b9896f7e10b6291cbfc4e7ace3ec28951817
mxxikr <mxxikr@gmail.com>
2026-02-10 17:18:13 +0900

    영속성 관리 내부 동작 방식: 영속성 컨텍스트 2

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,33 @@ public class JpaMain {
         try {
             // 비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(102L);
             member.setName("HelloA");
 
             // 영속
             System.out.println("=== BEFORE ===");
             entityManager.persist(member);
             System.out.println("=== AFTER ===");
+
+            Member findMember = entityManager.find(Member.class, 102L);
+
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            Member findMember2 = entityManager.find(Member.class, 100L);
+            Member findMember3 = entityManager.find(Member.class, 100L);
+
+            System.out.println("result = " + (findMember2 == findMember3));
+
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            entityManager.persist(member1);
+            entityManager.persist(member2);
+
+            Member member3 = entityManager.find(Member.class, 150L);
+            member3.setName("ZZZZZZ");
+            System.out.println("============================");
 
             tx.commit();
         } catch (Exception e) {
