bf7b5da320b8aaff423396d4650c70e20b4a3017
mxxikr <mxxikr@gmail.com>
2026-02-12 15:46:46 +0900

    다양한 연관관계 매핑: 실전예제 3 - 다양한 연관관계 매핑

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String city;
+    private String street;
+    private String zipCode;
+    private DeliveryStatus status;
+
+    @OneToOne
+    @JoinColumn(name = "delivery")
+    private Order order;
+}
